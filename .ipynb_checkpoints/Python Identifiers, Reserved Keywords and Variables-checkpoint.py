5eae5a30d3534658702b74de2e131203fd866555
Noon MIA Fatah Elrahman <46266930+nyXiNsane@users.noreply.github.com>
2020-04-17 14:52:56 +0200

    .

---
+++ .ipynb_checkpoints/Python Identifiers, Reserved Keywords and Variables-checkpoint.py
@@ -0,0 +1,31 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.4.0
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# # Python Identifiers
+
+# +
+import keyword
+
+print(keyword.kwlist)
+# -
+
+print(len(keyword.kwlist))
+
+# +
+rollnumber = 12345
+name = "noon"
+marks = 80.5
+
+print(rollnumber)
