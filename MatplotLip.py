88d641baddb119cdab66b2af0d63cb1a3c99d76f
Noon Abdulqadir <46266930+noon-abdulqadir@users.noreply.github.com>
2020-11-22 14:47:40 +0100

    .

--- MatplotLip.py
+++ MatplotLip.py
@@ -6,9 +6,9 @@
 #       extension: .py
 #       format_name: light
 #       format_version: '1.5'
-#       jupytext_version: 1.4.0
+#       jupytext_version: 1.6.0
 #   kernelspec:
-#     display_name: Python 3
+#     display_name: Python 3.9
 #     language: python
 #     name: python3
 # ---
